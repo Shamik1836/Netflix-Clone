12a44e9c36f51f61aef8f13ad9b9e01eb1bf0ecc
Shamik1836 <shamikdas2001@gmail.com>
2021-10-28 16:05:16 +0530

    Completed making the row component and fetching all the movies uing the tmdb database

---
+++ src/components/Row.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import styled from "styled-components";
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  // A snippet of code that runs based on some condition
+  useEffect(() => {
+    // if [], it run once the row loads first
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //   console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.table(movies);
+  return (
+    <div>
+      <h2>{title}</h2>
+      <RowPosters>
+        {movies.map((movie) =>
+          isLargeRow ? (
+            <RowPosterImgLarge
+              key={movie.id}
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            ></RowPosterImgLarge>
+          ) : (
+            <RowPosterImg
+              key={movie.id}
+              src={`${base_url}${movie.backdrop_path} `}
+              alt={movie.name}
+            ></RowPosterImg>
+          )
+        )}
+      </RowPosters>
+    </div>
+  );
+}
+
+export default Row;
+const RowPosterImg = styled.img`
+  width: 100%;
+  max-height: 100px;
+  object-fit: contain;
+  margin-right: 20px;
+  transition: transform 450px;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.08);
+  }
+`;
+const RowPosters = styled.div`
+  display: flex;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  padding: 20px;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+const RowPosterImgLarge = styled(RowPosterImg)`
+  max-height: 250px;
+  &:hover {
+    transform: scale(1.09);
+  }
+`;
